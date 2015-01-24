d7ba5fcbb375dab9219c7fead80a72ce4043b734
Ivan Shikhalev <shikhalev@gmail.com>
2015-01-25 01:57:27 +0500

    Build #0.1.0.3

---
+++ lib/set_version.rb
@@ -0,0 +1,70 @@
+# encoding: utf-8
+
+require 'rubygems'
+require 'digest/sha1'
+
+class Gem::Specification
+
+  SET_VERSION_VERSION = [0, 1]
+
+  def set_version *vers
+
+    if Integer === vers[-1]
+      beta = ''
+    else
+      beta = vers.pop.to_s
+    end
+    major = vers[0] || 0
+    minor = vers[1] || 0
+    patch = vers[2] || 0
+    key = "#{major}.#{minor}.#{patch}.#{beta}"
+
+    s_fn = caller_locations[0].absolute_path
+    s_nm = File.basename caller_locations[0].path, '.gemspec'
+    v_fn = File.expand_path "../#{s_nm}.gemvers", s_fn
+    if File.exist? v_fn
+      data = eval(File.read v_fn)
+    else
+      data = {}
+    end
+    stored = data[key]
+    if !stored
+      stored = {}
+    end
+    build = stored[:build] || 0
+
+    digest = Digest::SHA1.new
+    digest << (File.read s_fn)
+    files.each do |f|
+      digest << (File.read f)
+    end
+
+    if digest.hexdigest != stored[:hash]
+      build += 1
+      stored[:hash] = digest.hexdigest
+      stored[:build] = build
+      data[key] = stored
+      File.open(v_fn, 'w') { |f| f.puts data.inspect }
+    end
+
+    if beta == '' && build == 0
+      if patch == 0
+        result = "#{major}.#{minor}"
+      else
+        result = "#{major}.#{minor}.#{patch}"
+      end
+    else
+      result = "#{major}.#{minor}.#{patch}.#{beta}#{build}"
+    end
+    self.version = result
+
+    if system "git ls-files #{s_fn} --error-unmatch > /dev/null 2>&1"
+      system "git add #{v_fn} > /dev/null 2>&1"
+      system "git commit -a -m 'Build ##{result}' > /dev/null 2>&1"
+    end
+
+    return result
+
+  end
+
+end
---
+++ set_version.gemspec
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+require_relative 'lib/set_version';
+
+Gem::Specification.new do |g|
+  g.name = File.basename __FILE__, ".gemspec"
+  g.summary = 'Make gem version with autoicrement build number'
+  g.author = 'Ivan Shikhalev'
+  g.email = 'shikhalev@gmail.com'
+  g.homepage = 'https://github.com/shikhalev/set_version'
+  g.description = g.summary + '.'
+  g.license = 'GNU LGPLv3'
+
+  g.files = ['lib/set_version.rb', 'README.md', 'LICENSE']
+
+  g.set_version(*SET_VERSION_VERSION)
+
+  g.require_path = 'lib'
+  g.required_ruby_version = '~> 2.0'
+end
+
